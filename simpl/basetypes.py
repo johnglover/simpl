f1d58ace511e1a788f08a8d7f3da71724ac48350
John Glover <glover.john@gmail.com>
2011-08-19 15:59:50 +0100

    Update copyright info

--- simpl/basetypes.py
+++ simpl/basetypes.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009 John Glover, National University of Ireland, Maynooth
+# Copyright (c) 2009-2011 John Glover, National University of Ireland, Maynooth
 #  
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

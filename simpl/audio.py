f3052e2b09449745843c5858e9524a87e73663de
John Glover <j@johnglover.net>
2012-10-03 09:54:37 +0200

    [audio] Update read_wav function: just return the first channel from multi-channel audio files.

--- simpl/audio.py
+++ simpl/audio.py
@@ -5,5 +5,10 @@ import simpl
 
 def read_wav(file):
     'return floating point values between -1 and 1'
-    audio = wav.read(file)
-    return np.asarray(audio[1], dtype=simpl.dtype) / 32768.0, audio[0]
+    sampling_rate, audio = wav.read(file)
+
+    # if wav file has more than 1 channel, just take the first one
+    if audio.ndim > 1:
+        audio = audio.T[0]
+
+    return np.asarray(audio, dtype=simpl.dtype) / 32768.0, sampling_rate

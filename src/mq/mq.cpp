8335cd729aa804964e414c04954a6e2b53378175
Charles K. Neimog <charlesneimog@outlook.com>
2022-09-21 14:18:01 -0300

    add pi for Windows

--- src/mq/mq.cpp
+++ src/mq/mq.cpp
@@ -1,5 +1,10 @@
 #include "mq.h"
 
+//  M_PI is not defined in MSVC
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 using namespace simpl;
 
 // ----------------------------------------------------------------------------
